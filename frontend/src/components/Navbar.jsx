765c053e4d71ece7bf17657a790ee6649e7e4691
Alexandre OUDIN <alex.o51@hotmail.com>
2023-05-31 17:04:07 +0200

    creat navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/adt-logo.png";
+import icon from "../assets/Ecotourisme_4X4.jpg";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <navbar className="nav">
+      <img className="logo" src={logo} alt="Logo ADT de la Marne" />
+      <ul className="liste">
+        <li className="items">Tableau de bord</li>
+        <li className="items">Créer</li>
+        <li className="items">Administateur</li>
+        <li className="items">Déconnexion</li>
+      </ul>
+      <img className="icon" src={icon} alt="icon ADT de la Marne" />
+    </navbar>
+  );
+}
