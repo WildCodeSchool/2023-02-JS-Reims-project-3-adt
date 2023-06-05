5720e50faeb7caa389fe5eb2dfc7d6a8074d3f30
Alex-5110 <alexpom@live.fr>
2023-06-05 15:22:06 +0200

    add route express for Question

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,17 +1,8 @@
 const express = require("express");
-const mysql = require("mysql");
 
 const router = express.Router();
 
 const questionControllers = require("./controllers/questionControllers");
-
-const optionBd = {
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "ADT",
-};
 
 router.get("/questions/family/:familyId", questionControllers.browseByFamily);
 router.post("/questions/family/:familyId", questionControllers.addByFamily);
