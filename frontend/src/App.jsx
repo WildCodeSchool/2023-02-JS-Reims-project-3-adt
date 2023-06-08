b26708cfbc82c6dc0465f84396e0d15aacff9ff3
Rinawaticodes <kyurei.kim@gmail.com>
2023-06-08 09:21:41 +0200

    merge complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Register from "./pages/Register";
-import "./App.css";
 import HomeUser from "./pages/HomeUser";
 import Scoring from "./components/Scoring";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/formulaire" element={<HomeUser />} />
-        <Route path="/" element={<Register />} />
         <Route path="/scoring" element={<Scoring />} />
       </Routes>
     </div>
