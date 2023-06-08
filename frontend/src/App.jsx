ef574a0d90d0930108d68385626b1d6fe71fadc4
khaliev <dev.adlan@icloud.com>
2023-06-08 16:11:37 +0200

    survey route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Register from "./pages/Register";
 import HomeUser from "./pages/HomeUser";
 import Scoring from "./components/Scoring";
+import Survey from "./components/Survey";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<HomeUser />} />
         <Route path="/scoring" element={<Scoring />} />
+        <Route path="/survey/:familyId" element={<Survey />} />
+        <Route path="/survey/:familyId/:questionId" element={<Survey />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/Survey.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Survey() {
+  //   const [families, setFamilies] = useState([]);
+
+  return (
+    <div>
+      <h1>Question</h1>
+    </div>
+  );
+}
+
+export default Survey;
