82a156b292c367392159f77303c388d370824dcb
Alex-5110 <alexpom@live.fr>
2023-06-14 11:40:42 +0200

    modif tooltip in database

--- backend/database.sql
+++ backend/database.sql
@@ -1,4 +1,4 @@
-create table user (
+create table `user` (
   id int(11) unsigned primary key not null AUTO_INCREMENT,
   username varchar(80) not null unique,
   email varchar(80) not null unique,
@@ -12,30 +12,30 @@ id int(11) unsigned primary key not null AUTO_INCREMENT,
 title varchar(100) not null
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO category (title) VALUES ('Energy'),('Eau'),('Gestion des dechets'),('Biodeversité'), ('Communication et Sensibilitaion'),('Responsabilité sociale') ;
+INSERT INTO category (title) VALUES ("Gestion de l'énergie"),("Gestion de l'eau"),("Gestion des déchets"),("Biodeversité"), ("Communication et Sensibilitaion"),('Responsabilité sociale') ;
 create table question (
   id int(11) unsigned primary key not null AUTO_INCREMENT,
-  content varchar(900) not null,
+  content text not null,
   mandatory_level varchar(80) not null,
+  tooltip_content text,
   category_id int unsigned not null,
-  info_bulles varchar(900),
-  CONSTRAINT fk_category_id 
+  CONSTRAINT fk_category_id
   FOREIGN KEY (category_id)
   REFERENCES category(id)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO question (content, mandatory_level, category_id, info_bulles) VALUES 
-  ('Entretien des espaces extérieurs sans produits phytosanitaires : herbicides, fongicides, insecticides, etc.', 'obligatoire', 4, NULL),
+INSERT INTO question (content, mandatory_level, category_id, tooltip_content) VALUES 
+  ("Entretien des espaces extérieurs sans produits phytosanitaires : herbicides, fongicides, insecticides, etc.", "obligatoire", 4, NULL),
 
-  ('Choix d''espèce végétales non invasives', 'obligatoire', 4, 'Certaines espèces exotiques envahissantes font l''objet d''une surveillance comme par exemple la renouée du Japon, l''herbe de la pampa, l''armoisie à feuille épi, etc.'),
+  ("Choix d'espèce végétales non invasives", "obligatoire", 4, "Certaines espèces exotiques envahissantes font l'objet d'une surveillance comme par exemple la renouée du Japon, l'herbe de la pampa, l'armoisie à feuille épi, etc."),
 
-  ('Les aménagements extérieurs ne doivent pas représenter de danger pour la biodiversité (ex. risque de noyade dans un bac récupérateur d''eau de pluie).', 'obligatoire', 4, NULL),
+  ("Les aménagements extérieurs ne doivent pas représenter de danger pour la biodiversité (ex. risque de noyade dans un bac récupérateur d'eau de pluie).", "obligatoire", 4, NULL),
 
-  ('Espèces végétales principalement locales avec un impact positif sur la biodiversité : mellifères, baies pour les oiseaux, etc', 'essentiel', 4, NULL),
+  ("Espèces végétales principalement locales avec un impact positif sur la biodiversité : mellifères, baies pour les oiseaux, etc", "essentiel", 4, NULL),
 
-  ('Aménagements en faveur de l''accueil de la biodiversité (au moins trois) parmi : nichoir, mangeoire (été/hiver), mare, refuge à hérisson, hôtel à insectes (disposé judicieusement), etc.', 'essentiel', 4, NULL),
+  ("Aménagements en faveur de l'accueil de la biodiversité (au moins trois) parmi : nichoir, mangeoire (été/hiver), mare, refuge à hérisson, hôtel à insectes (disposé judicieusement), etc.", "essentiel", 4, NULL),
 
-  ('Engagement dans un réseau / label en faveur de la biodiversité', 'Optionnel', 4, 'P. ex : LPO');
+  ("Engagement dans un réseau / label en faveur de la biodiversité", "Optionnel", 4, "P. ex : LPO");
 
 
 
