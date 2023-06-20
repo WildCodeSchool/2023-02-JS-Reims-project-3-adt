296b4d038ba9589ab99f90dd7cd69d8db5371add
Rinawaticodes <kyurei.kim@gmail.com>
2023-06-20 11:04:38 +0200

    add category component

---
+++ frontend/src/components/Categories.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
+        }/categories`
+      )
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <section>
+      <div>
+        {categories.map((category) => (
+          <Link key={category.id} to={`/categories/${category.id}`}>
+            {category.title}
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Categories;
--- frontend/src/pages/Survey.jsx
+++ frontend/src/pages/Survey.jsx
@@ -1,32 +1,38 @@
-import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import { Link } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
 import NavbarUser from "../components/NavbarUser";
 import FooterUser from "../components/FooterUser";
+import Question from "./Question";
+import Categories from "../components/Categories";
 
 function Survey() {
-  const [categories, setCategories] = useState([]);
+  // const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/api/category")
-      .then((response) => {
-        setCategories(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/category")
+  //     .then((response) => {
+  //       setCategories(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <div>
       <NavbarUser />
       <h2>HEBERGEMENT</h2>
-      {categories.map((category) => (
+
+      {/* {categories.map((category) => (
         <Link key={category.id} to={`/survey/${category.id}`}>
+          <li>{category.title}</li>
           <button type="button">{category.title}</button>
         </Link>
-      ))}
+      ))} */}
+      <Categories />
+      <Question />
       <FooterUser />
     </div>
   );
