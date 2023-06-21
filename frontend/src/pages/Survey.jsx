a8fc475583eea733c2724a08be2106885b5b48a3
Rinawaticodes <kyurei.kim@gmail.com>
2023-06-21 16:33:28 +0200

    delete code useless

--- frontend/src/pages/Survey.jsx
+++ frontend/src/pages/Survey.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
 import NavbarUser from "../components/NavbarUser";
 import FooterUser from "../components/FooterUser";
 import Question from "../components/Question";
 import Categories from "../components/Categories";
 
 function Survey() {
-  const [indexPage, setIndexPage] = useState([]);
-
   return (
     <div>
       <NavbarUser />
-      <Categories setIndexPage={setIndexPage} />
-      <Question indexPage={indexPage} />
-
+      <Categories />
+      <Question />
       <FooterUser />
     </div>
   );
