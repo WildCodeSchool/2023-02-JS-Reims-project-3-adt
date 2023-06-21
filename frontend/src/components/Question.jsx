4f42e27cee63ca5ef3497da8db0c0290a67fc359
Rinawaticodes <kyurei.kim@gmail.com>
2023-06-21 10:50:07 +0200

    commit error

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -67,38 +67,4 @@ function Question() {
     </div>
   );
 }
-
-export default Question;
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-
-function Question() {
-  const [questions, setQuestions] = useState([]);
-  const { categoryId } = useParams();
-
-  useEffect(() => {
-    axios
-      .get(
-        `${
-          import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
-        }/categories/${categoryId}/questions`
-      )
-      .then((response) => setQuestions(response.data))
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [categoryId]);
-  return (
-    <div>
-      {questions.map((question) => (
-        <div key={question.id}>
-          <p>{question.content}</p>
-          <p>{question.mandatory_level}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default Question;
