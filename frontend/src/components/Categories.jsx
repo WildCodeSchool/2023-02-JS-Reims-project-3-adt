9d00590452047c3afcbb64017b7dae33b8c3aff9
Rinawaticodes <kyurei.kim@gmail.com>
2023-06-23 10:12:39 +0200

    delete section in categories.jsx

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -21,23 +21,21 @@ function Categories() {
   }, []);
 
   return (
-    <section>
-      <figure>
-        {categories.map((category) => (
-          <Link key={category.id} to={`/categories/${category.id}`}>
-            <figure>
-              <img
-                src={`${
-                  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
-                }${category.image}`}
-                alt={category.title}
-              />
-              <figcaption>{category.title}</figcaption>
-            </figure>
-          </Link>
-        ))}
-      </figure>
-    </section>
+    <figure>
+      {categories.map((category) => (
+        <Link key={category.id} to={`/categories/${category.id}`}>
+          <figure>
+            <img
+              src={`${
+                import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
+              }${category.image}`}
+              alt={category.title}
+            />
+            <figcaption>{category.title}</figcaption>
+          </figure>
+        </Link>
+      ))}
+    </figure>
   );
 }
 
