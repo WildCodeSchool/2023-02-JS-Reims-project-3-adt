e1c59ac15ab4b0c70d6ba26844e646052f3856e7
khaliev <dev.adlan@icloud.com>
2023-06-23 09:21:52 +0200

    sql db fixed

--- backend/database.sql
+++ backend/database.sql
@@ -27,52 +27,6 @@ create table question (
 
 INSERT INTO question (content, mandatory_level, category_id, tooltip_content) VALUES 
 
-<<<<<<< HEAD
-  ("Suivi mensuel des consommations électriques", "obligatoire", 1, "Seules les consommations liées à l'activité touristique doivent être comptablilisées."),
-
-  ("Isolation performante des parois vitrées : double ou triple vitrage sur au moins 90% des ouvertures.
-Pour les  constructions neuves ou rénovées, la totalité des ouvertures doit être équipée.", "obligatoire", 1, NULL),
-
-  ("Plus de la moitié des dispositifs d'éclairage sont de classe A", "obligatoire", 1, "Eclairage à LED dans les pièces intérieures et les extérieurs."),
-
-  ("L'hébergement n'a pas recours à des dispositifs de chauffage ou de climatisation extérieurs","obligatoire", 1, "P. ex : parasol chauffant, brumisateur de terrasse, etc."),
-
-  ("Chauffage à faible consommation : chaudière gaz à condensation, PAC à air classe A+ mini,  etc.", "obligatoire", 1, "L'étiquette énergie des Pompes à Chaleur (PAC) dépend de nombreux paramètres. Vous retrouverez toutes les informations de votre matériel sur le site du fabricant."),
-
-  ("Appareils électroménagers performants selon les recommandations de l'ADEME (cf. infographie de l'ADEME)
-A minima 3 équipements suivants : Réfrigérateur, lave-linge, sèche-linge, lave-vaisselle, four.", "essentiel", 1, "Une infographie de l'ADEME permet d'identifier facilement les classes recommandées. Elle est accessible à cette adresse : https://agirpourlatransition.ademe.fr/particuliers/conso/droit-conso/nouvelle-etiquette-energie."),
-
-  ("Pour les constructions neuves ou rénovation totale : utilisation de matériaux de construction biosourcés.", "essentiel", 1, "Les matériaux de construction biosourcés sont par exemple le bois, les panneaux type Fermacell®, les enduits terre, etc. "),
-
-  ("Isolation thermique performante des murs et plafonds
-https://librairie.ademe.fr/cadic/2047/guide-pratique-isoler-sa-maison.pdf?modal=false", "essentiel", 1, "La résistance thermique applicable est de :
-  > toiture (comples perdus ou rampants)  r>= 6 m². K/W
-  > murs (isolation intérieure ou extérieure) r>= 3,7 m². K/W"),
-
-  ("Les parois vitrées exposées au soleil doivent pouvoir être occultées en été : volets battants, électriques, casquette végétale, etc.", "essentiel", 1, NULL),
-
-  ("Pour ses besoins en chauffage ou eau chaude sanitaire, l'établissement 
-  - utilise des énergies renouvelables
-ou 
-  - est alimenté en électricité par un fournisseur labellisé Vert Volt
-https://agirpourlatransition.ademe.fr/particuliers/vertvolt#paragraph-584788", "essentiel", 1, "A minima pour le chauffage ou la production d'eau chaude sanitaire (ECS). Un ballon d'eau chaude sanitaire thermodynamique satisfait ce critère. Attention à bien dimensionner ses besoins en ECS."),
-  ("Possibilité de régulation différenciée du chauffage selon la période ou les pièces", "essentiel", 1, NULL),
-
-  ("L'éclairage d'espaces communs est équipé de détecteur de mouvement. Les minuteries sont à éviter pour les personnes porteuses de handicap.", "essentiel", 1, "Un niveau d'éclairage minimum pour des questions de sécurité des biens ou des personnes peut être prévu. Il devra être dimensionné en conséquence de manière à s'adapter à la présence ou non de personnes."),
-
-  ("Les besoins en électricité sont couverts par 
-  - un mode de production basé sur les énergies renouvelables 
-ou
-  -  un fournisseur labellisé Vert Volt
-https://agirpourlatransition.ademe.fr/particuliers/vertvolt#paragraph-584788", "essentiel", 1, NULL),
-
-  ("Conception du bâtiment avec un niveau de performance élevé : Haute Qualité Environnementale, bioclimatique, BBC, bâtiment passif ou à énergie positive.", "optionnel", 1, NULL),
-  
-  ("Matériaux d'isolation biosourcés", "optionnel", 1, "Les matériaux d'isoaltion biosourcés peuvent être d'origine végétale (laine de bois, laine de chanvre, etc.), animale (laine de mouton, etc) ou issus du recyclage (textile p.ex.)."),
-
-
- ("Mise en place du tri des déchets selon les modes de collecte locaux : papier, plastiques, métal, verre", "Obligatoire", 3, NULL),
-=======
   ("Suivi mensuel des consommations électriques.", "Obligatoire", 1, "Seules les consommations liées à l'activité touristique doivent être comptablilisées."),
     
   ("Isolation performante des parois vitrées : double ou triple vitrage sur au moins 90% des ouvertures.
@@ -141,7 +95,6 @@ https://agirpourlatransition.ademe.fr/particuliers/vertvolt#paragraph-584788", "
       - pourvus d'une gestion optimisée du chlore. ", "Optionnel", 2, NULL),
  
  ("Mise en place du tri des déchets selon les modes de collecte locaux : papier, plastiques, métal, verre.", "Obligatoire", 3, NULL),
->>>>>>> 32300cd426dde097598f3366a9ae739e570e2728
 
  ("Aucun produit jetable n'est proposé ou un plan de suppresssion est proposé pour :
     >  les articles de toilette
@@ -175,8 +128,6 @@ https://agirpourlatransition.ademe.fr/particuliers/vertvolt#paragraph-584788", "
 
   ("Affichage de l'engagement en faveur de l'écotourisme : signalétique extérieure, site internet, support print.", "Obligatoire", 5, NULL),
 
-<<<<<<< HEAD
-=======
   ("Sensibiliser les clients sur leur rôle  : économies d'énergies, d'eau, réduction des déchets, etc.", "Obligatoire", 5, NULL),
 
   ("Le personnel est formé à la politique environnementale de l'établissement : engagements, gestes et/ou procédures (eau, énergie, etc.).", "Obligatoire", 5, NULL),
@@ -198,7 +149,6 @@ https://agirpourlatransition.ademe.fr/particuliers/vertvolt#paragraph-584788", "
 
   ("L'établissement est labellisé pour l'accueil de personnes en situation de handicap.", "Optionnel", 6, NULL);
 
->>>>>>> 32300cd426dde097598f3366a9ae739e570e2728
 create table user_survey (
   id int(11) unsigned primary key not null AUTO_INCREMENT,
   user_id int unsigned not null,
