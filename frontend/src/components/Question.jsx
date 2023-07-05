027078861437c560ec553d9dd4f769d944f4a016
Rinawaticodes <kyurei.kim@gmail.com>
2023-07-05 16:17:08 +0200

    resolved PR

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -40,8 +40,8 @@ function Question() {
           );
         })
         .map((question) => (
-          <div className="questions">
-            <div key={question.id} className="questionList">
+          <div key={question.id} className="questions">
+            <div className="questionList">
               <p className="questionContent">{question.content}</p>
               <p className="mandatoryLevel">{question.mandatory_level}</p>
             </div>
