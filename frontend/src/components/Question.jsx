d7b08728b141843d3a3ea3f67c88a11d814e4d69
Rinawaticodes <kyurei.kim@gmail.com>
2023-07-06 10:10:34 +0200

    delete import

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Question.css";
 
