99c1e41eade9acad75b00733b60639af626f663e
Elodie0530 <uneondine@gmail.com>
2023-07-24 20:40:37 +0200

    style sidebar dashboard

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import "../styles/Navbar.css";
+import "../styles/Dashboard.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <button type="button" className="">
-        {" "}
-        eee{" "}
-      </button>
-      <button type="button" className="">
-        {" "}
-        eeeer{" "}
-      </button>
-      <button type="button" className="">
-        {" "}
-        eee{" "}
-      </button>
-
-      <button type="button" className="deconectionadmin">
-        {" "}
-      </button>
+    <div className="btnSidebar">
+      <button type="button">Ajouter</button>
+      <button type="button">Modifier</button>
+      <button type="button">Supprimer</button>
+      <div>
+        <button type="button" className="deconectionadmin">
+          Déconnection
+        </button>
+      </div>
     </div>
   );
 }
