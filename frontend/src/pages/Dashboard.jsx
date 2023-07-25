d2263cda24496e39d9e4b1fd00e1240ce23d51c8
khaliev <dev.adlan@icloud.com>
2023-07-25 12:06:20 +0200

    dashboard styling

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,17 @@
 import Headeradmin from "../components/HeaderAdmin";
+import FilterBar from "../components/FilterBar";
 import Sidebar from "../components/Sidebar";
+import FooterUser from "../components/FooterUser";
 import "../styles/Dashboard.css";
 
 function Dashboard() {
   return (
-    <>
+    <div className="dashboard-wrapper">
       <Headeradmin />
-      <Sidebar />{" "}
-    </>
+      <Sidebar />
+      <FilterBar />
+      <FooterUser />
+    </div>
   );
 }
 
